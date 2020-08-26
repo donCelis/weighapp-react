e4e3decd3eaa2f51a542669d437dbb8398e09cf9
Alexander Celis <dacelis0@misena.edu.co>
2020-08-26 13:02:51 -0500

    Updates

---
+++ precache-manifest.6068f502be90189b63afb2526cc7e496.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8590403c04ba0a11a6bc1f65551dd92f",
+    "url": "/weighapp-react/index.html"
+  },
+  {
+    "revision": "0d72f7c7d03485263a05",
+    "url": "/weighapp-react/static/css/2.7580c1a4.chunk.css"
+  },
+  {
+    "revision": "fd86eb7f7de480fc428c",
+    "url": "/weighapp-react/static/css/main.db286e09.chunk.css"
+  },
+  {
+    "revision": "0d72f7c7d03485263a05",
+    "url": "/weighapp-react/static/js/2.62631532.chunk.js"
+  },
+  {
+    "revision": "0b08544f54c9906705be39c03b7e79ad",
+    "url": "/weighapp-react/static/js/2.62631532.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fd86eb7f7de480fc428c",
+    "url": "/weighapp-react/static/js/main.e4149d0d.chunk.js"
+  },
+  {
+    "revision": "e0e5933504c0a3c6b3e9",
+    "url": "/weighapp-react/static/js/runtime-main.8f5d4acf.js"
+  }
+]);
