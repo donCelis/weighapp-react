dcdc05fd58c51d343978106e66023918ae72be91
Alexander Celis <dacelis0@misena.edu.co>
2020-08-26 12:54:54 -0500

    deploy build

---
+++ build/precache-manifest.f18aa197853795d8ef84ab4564fba67b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f5b6f3488f82bb038d9b9e61b770f1de",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d8e5c7bc2b8f1d529196",
+    "url": "/static/css/2.7580c1a4.chunk.css"
+  },
+  {
+    "revision": "fd86eb7f7de480fc428c",
+    "url": "/static/css/main.db286e09.chunk.css"
+  },
+  {
+    "revision": "d8e5c7bc2b8f1d529196",
+    "url": "/static/js/2.b10eb752.chunk.js"
+  },
+  {
+    "revision": "0b08544f54c9906705be39c03b7e79ad",
+    "url": "/static/js/2.b10eb752.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fd86eb7f7de480fc428c",
+    "url": "/static/js/main.e4149d0d.chunk.js"
+  },
+  {
+    "revision": "34b69727cd655103b3b2",
+    "url": "/static/js/runtime-main.e7bdc282.js"
+  }
+]);
--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,9 @@
 import React from 'react';
 import { render } from '@testing-library/react';
-import App from './App';
+import Router from './Routes/Router';
 
 test('renders learn react link', () => {
-  const { getByText } = render(<App />);
+  const { getByText } = render(<Router />);
   const linkElement = getByText(/learn react/i);
   expect(linkElement).toBeInTheDocument();
 });
