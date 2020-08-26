f4283ef283830f85a95286230db38c166aa6519a
Alexander Celis <dacelis0@misena.edu.co>
2020-08-26 15:22:34 -0500

    chaged gitignore

--- src/Components/preloader.jsx
+++ src/Components/preloader.jsx
@@ -2,16 +2,18 @@ import React, { useState, useEffect } from "react";
 import { Circle } from "react-preloaders";
 
 const Preloader = () => {
-  
   const [loading, setLoading] = useState(true);
 
   useEffect(() => {
-    setTimeout(() => setLoading(false), 500);
+    setTimeout(() => {
+      setLoading(false);
+    }, 800);
   }, []);
 
   return (
     <Circle
       customLoading={loading}
+      time={0}
       color={"#2980b9"}
       background="#2c3e50"
       animation="fade"
