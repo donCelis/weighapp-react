7a04ddd21a6293ba5e7d77a14f5eab1b1f2a3d76
Alexander Celis <dacelis0@misena.edu.co>
2020-08-26 14:04:24 -0500

    Updates

---
+++ precache-manifest.d7cd95b07b96483cfa09e376b7149662.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "20d6864571225f3959e4a683c25bf1bf",
+    "url": "./index.html"
+  },
+  {
+    "revision": "26bd33b4ec6e1b701a9d",
+    "url": "./static/css/2.7580c1a4.chunk.css"
+  },
+  {
+    "revision": "f649dfecf907a4594c33",
+    "url": "./static/css/main.db286e09.chunk.css"
+  },
+  {
+    "revision": "26bd33b4ec6e1b701a9d",
+    "url": "./static/js/2.6b466853.chunk.js"
+  },
+  {
+    "revision": "0b08544f54c9906705be39c03b7e79ad",
+    "url": "./static/js/2.6b466853.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f649dfecf907a4594c33",
+    "url": "./static/js/main.a7960396.chunk.js"
+  },
+  {
+    "revision": "c2c6b1bc4901ef6cca07",
+    "url": "./static/js/runtime-main.bb137d90.js"
+  }
+]);
